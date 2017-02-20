20df887bda2fa15877333d8156604742d54a26ec
Jacob Dorpinghaus <jacob.dorpinghaus@gmail.com>
2017-02-19 23:21:47 -0600

    Added command line argument loop with error checking

--- src/lab4.c
+++ src/lab4.c
@@ -11,19 +11,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <strings.h>
+#include <string.h>
 
 int* fileToArray(int num);
+void printUsage(void);
 
 int main(int argc, char* argv[]) {
 	int* array;
-	array = fileToArray(11);
+	char* newFilename;
+	int i, j, stringLength;
+	float offset, scale;
+	for(i=0; i<argc; i++){
+		if(argv[i][0] == '-'){
+			switch(argv[i][1]){
+			case 'n':
+				if(atoi(argv[i+1]) <= 11 && atoi(argv[i+1]) >= 1){
+					array = fileToArray(atoi(argv[i+1]));
+				} else {
+					printUsage();
+					exit(0);
+				}
+				break;
+			case 'o':
+				if (sscanf(argv[i+1], "%f", &offset) == 0){ //check that a valid float value was input
+					printUsage();
+					exit(0);
+				}
+				break;
+			case 's':
+				if (sscanf(argv[i+1], "%f", &scale) == 0){ //check that a valid float value was input
+					printUsage();
+					exit(0);
+				}
+				break;
+			case 'r':
+				stringLength = strlen(argv[i+1]);
+				printf("\n\nlen: %d\n\n", stringLength);
+				if((argv[i+1][stringLength-1]!='t')||(argv[i+1][stringLength-2]!='x')||(argv[i+1][stringLength-3]!='t')||(argv[i+1][stringLength-4]!='.')){ //Check that a valid .txt filename was entered
+					printUsage();
+					exit(0);
+				}
+				break;
+			case 'h':
+				break;
+			case 'S':
+				break;
+			case 'C':
+				break;
+			case 'N':
+				break;
+			default:
+				printUsage();
+				exit(0);
+				break;
+			}
+
+		}
+	}
+	free(array);
 	return EXIT_SUCCESS;
 }
 
-int* filetoArray(int num){
+int* fileToArray(int num){
 	int* dataArray;
-	int numItems, numMax, currentNum;
+	int numItems, numMax;
 	int x=0;
 	FILE *fp;
 	char snum[3];
@@ -35,5 +86,18 @@ int* filetoArray(int num){
 	fscanf(fp, "%d %d\n", &numItems, &numMax); //read in number of values and maximum value
 	dataArray = calloc(numItems, sizeof(int)); //allocate memory for number of integers
 	while(fscanf(fp, "%d", &dataArray[x++]) != EOF){} //scan values into integer array
+	fclose(fp);
 	return dataArray;
+}
+
+void printUsage(){
+	printf("\nUsage:\n");
+	printf("-n <file number>\tNumber of file to open, between 1 and 11\n");
+	printf("-o <offset value>\tOffset the data by the given value\n");
+	printf("-s <scale factor>\tScale the data by the given scaling factor\n");
+	printf("-r <new filename>\tCreate a copy of the file with the given filename\n");
+	printf("-h\t\t\tDisplay usage instructions\n");
+	printf("-S\t\t\tDisplay data statistics\n");
+	printf("-C\t\t\tCenter the data\n");
+	printf("-N\t\t\tNormalize the data\n");
 }
